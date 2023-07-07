c98ecc496aa84373ce84233cc7d6d97ee32cee96
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2023-07-07 19:26:20 -0300

    add: practicing with headers

--- src/main/java/com/deyvidsalvatore/praticaljava/server/DefaultRestApi.java
+++ src/main/java/com/deyvidsalvatore/praticaljava/server/DefaultRestApi.java
@@ -3,7 +3,9 @@ package com.deyvidsalvatore.praticaljava.server;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +28,19 @@ public class DefaultRestApi {
         return LocalTime.now().toString();
     }
 
+    @GetMapping("/headerOne")
+    public String headerByAnnotation(
+            @RequestHeader(name = "User-agent", required = false) String headerUserAgent,
+            @RequestHeader(name = "Practical-java", required = false) String headerPracticalJava) {
+        return "User-agent: " + headerUserAgent + ", Practical-java : " + headerPracticalJava;
+    }
+
+    @GetMapping("/headerTwo")
+    public String headerByRequest(
+        ServerHttpRequest request
+    ) {
+        return "User-agent: " + request.getHeaders().getValuesAsList("User-agent") +
+                ", Pratical-java : " +
+                request.getHeaders().getValuesAsList("Practical-java");
+    }
 }
